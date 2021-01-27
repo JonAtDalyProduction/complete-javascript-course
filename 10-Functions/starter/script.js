1578c0ce0375160df5a032f5c6802ef53b70f92c
JonAtDalyProduction <71096537+JonAtDalyProduction@users.noreply.github.com>
2021-01-26 16:35:09 -0800

    updated

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,2 +1,375 @@
-'use strict';
 console.log("server live");
+'use strict';
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+// LESSON 7===============================
+// ########## FUNCTION CLOSURES ###############
+// EXAMPLE 2 
+/*
+const boardPassengers = function(n, wait) {
+    const perGroup = n / 3;
+    setTimeout(() => {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+    console.log(`Will start boarding in ${wait} seconds`);
+}
+boardPassengers(180, 3)
+*/
+// EXAMPLE 1 
+/*
+let f; 
+const g = function() {
+    const a = 23; 
+    f= function() {
+        console.log(a * 2);
+    }
+}
+
+const h = function() {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    }
+}
+g()
+f()
+// re-assinging the f function
+console.dir(f)
+h()
+f()
+console.dir(f)
+*/
+/*
+const secureBooking = function() {
+    let passengerCount = 0;
+    return function () {
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    }
+}; 
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+console.dir(booker)
+*/
+// LESSON 7===============================
+/*
+// LESSON 6===============================
+// ########## IIFE FUNCTION ###############
+// variables inside the IIFE are not avaiable in the global scope 
+(function () {
+    const isPrivate = 'This variable is only available inside this function.'
+    console.log(`This is an IIFE! It will never run again! ${isPrivate}`);
+})();
+// This is an IIFE! It will never run again! This variable is only available inside this function.
+// console.log(isPrivate);
+// Uncaught ReferenceError: isPrivate is not defined
+{
+    const isPrivate = 'This const is private';
+    var notPrivate = 'This var is not private';
+}
+//This is an arrow version of IIFE!
+// (() => console.log(`This is an arrow version of IIFE! ${isPrivate}`))();
+// // Uncaught ReferenceError: isPrivate is not defined
+(() => console.log(`This is an arrow version of IIFE! ${notPrivate}`))();
+// This is an arrow version of IIFE! This var is not private
+// LESSON 6===============================
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the
+ number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. 
+        The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example,
+   if the option is 3, increase the value AT POSITION 3 of the array by 1. 
+   Make sure to check if the input is a number and if the number makes sense 
+   (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. 
+    The method takes a string as an input (called 'type'), which can be either 
+    'string' or 'array'. If type is 'array', simply display the results array as
+     it is, using console.log(). This should be the default option. If type is 'string',
+    display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. 
+        Use both the 'array' and the 'string' option. Do NOT put the arrays in the
+        poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+/*
+const poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section 😃
+    answers: new Array(4).fill(0),
+    registerNewAnswer(){
+        const answer = Number(
+            prompt(`${this.question}\n${this.options.join('\n')}
+            \n(write option number)`
+            )
+        ); 
+        console.log(answer);
+        typeof answer === 'number' && answer < this.answers.length 
+        && this.answers[answer]++;
+        console.log(this.answers);
+        this.displayResults();
+        this.displayResults('string');
+    },
+
+    displayResults (type = 'array'){
+        if (type === 'array') {
+            console.log(this.answers);
+        } else if (type === 'string') {
+         console.log(`Poll results are ${this.answers.join(',')} `);
+        }
+    },
+};
+
+// console.log(poll.displayResults);
+
+document
+.querySelector('.poll')
+.addEventListener('click', poll.registerNewAnswer
+.bind(poll));
+
+
+poll.displayResults.call({answers: [5, 2, 3] })
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1] }, 'string')
+// console.log(displayResults.call(testData1));
+*/
+/*
+// LESSON 5===============================
+const lufthansa = {
+    airline: 'Lufthansa',
+    iata: 'LH',
+    bookings: [],
+
+    book(flightNum, name) {
+        this.bookings.push({flight: `${this.iata}${flightNum}`, name})
+        console.log(`${name} booked ${this.airline} flight ${this.iata}${flightNum}`);
+    },
+};
+const eurowings = {
+    airline: 'EuroWings',
+    iata: 'EW',
+    bookings: [],
+};
+const swiss = {
+    airline: 'Swiss Airlines',
+    iata: 'LX',
+    bookings: [],
+};
+const book = lufthansa.book;
+// ########## BIND METHOD ###############
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(423, 'Phil Michelson')
+// Phil Michelson booked EuroWings flight EW423
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Dustin Johnson');
+// Dustin Johnson booked EuroWings flight EW23
+
+lufthansa.planes = 300
+lufthansa.buyPlane = function () {
+    console.log(this);
+    this.planes++;
+    console.log(this.planes);
+};
+document.querySelector('.buy').addEventListener
+('click', lufthansa.buyPlane.bind(lufthansa));
+// without BIND METHOD 
+// <button class=​"buy">​Buy new plane 🛩​</button>​
+// WITH BIND METHOD 
+// {airline: "Lufthansa", iata: "LH", bookings: Array(0), planes: 300, book: ƒ, …}airline: "Lufthansa"book: ƒ book(flightNum, name)arguments: (...)caller: (...)length: 2name: "book"__proto__: ƒ ()[[FunctionLocation]]: script.js:9[[Scopes]]: Scopes[2]bookings: []buyPlane: ƒ ()iata: "LH"planes: 301__proto__: Object
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+// 220
+const addVAT = addTax.bind(null, 0.23)
+console.log(addVAT(100));
+// 123
+// LESSON 5===============================
+*/
+/*
+// LESSON 4=============================== 
+const lufthansa = {
+    airline: 'Lufthansa',
+    iata: 'LH',
+    bookings: [],
+
+    book(flightNum, name) {
+        this.bookings.push({flight: `${this.iata}${flightNum}`, name})
+        console.log(`${name} booked ${this.airline} flight ${this.iata}${flightNum}`);
+    },
+};
+lufthansa.book(239, 'John Daly');
+// John Daly booked Lufthansa flight LH239
+lufthansa.book(240, 'Tiger Woods')
+// Tiger Woods booked Lufthansa flight LH240
+console.log(lufthansa);
+// {airline: "Lufthansa", iata: "LH", bookings: Array(2), book: ƒ}
+const eurowings = {
+    airline: 'EuroWings',
+    iata: 'EW',
+    bookings: [],
+};
+const swiss = {
+    airline: 'Swiss Airlines',
+    iata: 'LX',
+    bookings: [],
+};
+const book = lufthansa.book;
+// book(123, 'John Doe');
+// uncaught TypeError: Cannot read property 'bookings' of undefined
+// ########## CALL METHOD ###############
+book.call(eurowings, 123, 'John Doe')
+// John Doe booked EuroWings flight EW123
+book.call(lufthansa, 240, 'Jordan Spieth')
+// Jordan Spieth booked Lufthansa flight LH240
+book.call(swiss, 567, 'Jason Day')
+// Jason Day booked Swiss Airlines flight LX567
+
+// ########## APPLY METHOD ###############
+const bookingData = [678, 'Rory Mcleroy']
+book.apply(swiss, bookingData)
+// Rory Mcleroy booked Swiss Airlines flight LX678
+//-------------SAME AS------------------------
+book.call(swiss, ...bookingData)
+// Rory Mcleroy booked Swiss Airlines flight LX678
+// LESSON 4=============================== 
+*/
+/*
+// LESSON 3=============================== 
+// longest way
+// const greet = function (greeting){
+//     return function(name) {
+//     console.log(`${greeting} ${name}`);
+//     }
+// }
+// shorter arrow 
+// const greet = gretting => {
+// return ((greeting, name) => console.log(`${greeting} ${name}`));
+// }
+// shortest arrow 
+const greet = gretting => name => console.log(`${greeting} ${name}`);
+
+const greeterHey = greet('Hey')
+greeterHey('John');
+// LESSON 3=============================== 
+*/
+/*
+// LESSON 2=============================== 
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+const transformer = function (str, fn) {
+    console.log(`Original String ${str}`);
+ console.log(`Transformed String ${fn(str)}`);
+ console.log(`Function ran: ${fn.name}`);
+}
+transformer('This is a String', upperFirstWord)
+transformer('This is a String', oneWord)
+
+const high5 = function () {
+    console.log("HIGH FIVE");
+}
+
+document.body.addEventListener('click', high5);
+['One', 'Two', 'Three'].forEach(high5);
+// LESSON 2===============================
+*/
+/*
+// LESSON 1=============================== 
+const bookings = [];
+// DEFAULT PARAMETER VALUES
+const createBooking = function (
+    flightNum,
+     numPassengers = 1,
+      price = 199 * numPassengers) {
+    //ES 5
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+    const booking = {
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking)
+    bookings.push(booking);
+}
+// createBooking('LH123')
+// createBooking('LH456', 2, 500)
+const flight = "LH234"
+const person = {
+    name: 'John Daly',
+    passport: 1234567890
+}
+const checkIn = function (flightNum, passenger) {
+ flightNum = 'LH999'
+ passenger.name = 'Mr. ' + passenger.name;
+ if(passenger.passport === 1234567890) { alert('Checked In')} 
+ else { alert('wrong passport number') }
+}
+const newPassport = function (person) {
+    person.passport = Math.trunc(Math.random() * 1000000000)
+};
+newPassport(person);
+checkIn(flight, person)
+console.log(flight);
+console.log(person);
+//LESSON 1===============================
+*/
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -3,7 +3,7 @@
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // BANKIST APP
-
+ 
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -35,6 +35,17 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+// create a first/last initial username from the account holders full name 
+const createUsername = (accs) => {
+  accs.forEach(function (acc) {
+  acc.username = acc.owner
+.toLowerCase()
+.split(' ')
+.map(name => name[0])
+.join('');
+});
+};
+createUsername(accounts);
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -61,16 +72,232 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
+///////////////////START ACTIVE APP CODE//////////////////////////////
+// display the balance in the upper right of the page
+const calcDisplayBalance = (acc) => {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `$${acc.balance}`
+}
+// display the summary values on the bottom left of the page
+const calcDisplaySummary = (acc) => {
+// calc and display positive movement values
+const credits = acc.movements.filter(mov => mov > 0)
+.reduce((acc, mov) => acc + mov, 0);
+labelSumIn.textContent= `$${credits}`;
+// calc and display negative movement values
+const debits = acc.movements.filter(mov => mov < 0)
+.reduce((acc, mov) => acc + mov, 0);
+labelSumOut.textContent= `$${Math.abs(debits)}`;
+// calc and display interest on deposits
+const interest = acc.movements.filter(mov => mov > 0)
+.map(deposit => (deposit * 1.2) / 100)
+.filter(int => int >= 1 ?? int)
+.reduce((acc, int) => acc + int, 0);
+labelSumInterest.textContent = `$${interest}`
+};
+// display the account transactions on the left pane of the page
+const displayMovements = function(acc) {
+  containerMovements.innerHTML= '';
+  acc.movements.forEach(function(mov, i){
+    const transType = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+    <div class="movements__type movements__type--${transType}">${i + 1} ${transType}</div>
+    <div class="movements__date">3 days ago</div>
+    <div class="movements__value">${mov}$</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+}
+// update the UI
+const updateUi = function (acc) {
+      // Display movements
+      displayMovements(acc);
+      // Display balance
+      calcDisplayBalance(acc);
+      // Display Summary 
+      calcDisplaySummary(acc);
+}
+// EVENT HANDLERS 
+let currentAccount;
+const loginHandler = btnLogin.addEventListener('click', function (e) {
+  // prevent form from submitting
+  e.preventDefault(); 
+  const currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // console.log(currentAccount); 
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back ${currentAccount.owner.split(' ')[0]}` 
+    containerApp.style.opacity = 100;
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+    updateUi(currentAccount);  
+  }
+
+});
+
+const transferHandler = btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  console.log(currentAccount);
+const amount = Number(inputTransferAmount.value);
+const recieverAcc = accounts.find(acc => acc.username === inputTransferTo.value);
+console.log(amount, recieverAcc);
+if (
+  amount > 0 &&
+  currentAccount.balance >= amount && /// RIGHT HERE ///
+  receiverAcc?.username !== currentAccount.username
+) {
+  console.log('Transfer Valid', amount, recieverAcc);
+  currentAccount.movements.push(-amount);
+  recieverAcc.movements.push(amount);
+  updateUi(currentAccount)
+} else {
+  console.log('Transfer Invalid', amount, recieverAcc);
+}
+});
+///////////////////END ACTIVE APP CODE//////////////////////////////
+
+//*****************START DEMO CODE********************************//
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const eurToUsd = 1.1; 
+
+const movementsUsd = movements.map(mov => mov * eurToUsd);
+
+const movDesc = movements.map((mov, i) => 
+ `Transaction ${i + 1}: ${mov > 0 ? 'Deposit' : 'Widthdrawl'} - ${Math.abs(mov)}`
+);
+
+const deposits = movements.filter(mov => mov > 0);
+const widthdrawls = movements.filter(mov => mov < 0);
+const max = movements.reduce((acc, mov) => acc > mov ? acc : mov, movements[0]);
+const min = movements.reduce((acc, mov) => acc < mov ? acc : mov, movements[0]);
+// console.log(max);
+// console.log(min);
+const calcAvgMov = (movs) => (movs.reduce((acc, mov) => acc + mov, 0) / movs.length);
+// *******METHOD CHANING******************
+const totalDepUsd = (movs) => 
+movs.filter(mov => mov > 0)
+.map(mov => mov * eurToUsd)
+.reduce((acc, mov) => acc + mov, 0)
+// console.log(totalDepUsd(movements))
+*/
+//*********INSPECT THE ARRAY IN THE CHAIN USING THE ARR CALLBACK */
+// movs.filter(mov => mov > 0)
+// .map((mov, i, arr) => {
+//   console.log(mov, i , arr);
+//   return mov * eurToUsd;
+// })
+// .reduce((acc, mov) => acc + mov, 0)
+// console.log(totalDepUsd(movements))
+  
+// 
+
+
+/*
+console.log('vvvvv-BALANCE LONG-vvvvv');
+const balanceLong = movements.reduce(function(acc, cur, i, arr){
+  console.log(i, acc);
+  return acc + cur;
+}, 0);
+console.log(balanceLong);
+*/
+// *******THE FIND METHOD******************
+//returns the first value that matches the condition
+/*
+const findFirstDebit = (movs) => movs.find(mov => mov < 0);
+console.log(movements);
+console.log(findFirstDebit(movements));
+console.log(accounts)
+*/
+// const findUser = (accs, user) => (accs.find(acc => acc.owner === user));
+// console.log(findUser(accounts, 'Sarah Smith'));
 /////////////////////////////////////////////////
 // LECTURES
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+// SLICE
+// console.log(arr.slice(2));
+// // (2) ["c", "d"]
+// console.log(arr.slice(2, 4));
+// // (3) ["c", "d", "e"]
+// console.log(arr.slice(-2));
+// // (2) ["d", "e"]
+// console.log(arr.slice(-1));
+// // ["e"]
+// console.log(arr.slice(1, -2));
+// // (2) ["b", "c"]
+// console.log(arr.slice());
+// // (5) ["a", "b", "c", "d", "e"]
+// console.log([...arr]);
+// // (5) ["a", "b", "c", "d", "e"]
 
-const currencies = new Map([
+// SPLICE
+// arr.splice(-1)
+// console.log(arr);
+// // (4) ["a", "b", "c", "d"]
+// arr.splice(1, 2)
+// console.log(arr);
+// // (2) ["a", "d"]
+// const arr2 =['j', 'i', 'h', 'g', 'f']
+// console.log(arr2.reverse());
+// // (5) ["f", "g", "h", "i", "j"]
+
+// CONCAT **SAME AS AS SPREAD OPPERATOR **
+// const letters = arr.concat(arr2);
+// console.log(letters);
+// // (10) ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+// console.log([...arr, ...arr2]);
+// // (10) ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+// JOIN 
+// console.log(letters.join(' - '));
+// // a - b - c - d - e - f - g - h - i - j
+
+/////////////////////////////////////////////////////////////////////////////////
+// ----------------FOR EACH WITH ARRAYS-----------------------------------------
+/*
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0 ){console.log(`Transaction #${i+1} Deposit- $${movement}`);}
+  else {console.log(`Transaction #${i+1} Withdrawl- $${Math.abs(movement)}`);}
+  
+}
+console.log('---------------FOREACH-------------------');
+movements.forEach(function(mov, i, arr){
+  if (mov > 0 ){console.log(`Transaction #${i+1} Deposit- $${mov}`);}
+  else {console.log(`Transaction #${i+1} Withdrawl- $${Math.abs(mov)}`);}
+});
+// {{FOREACH}} WILL ALWAYS LOOP THE ENTIRE ARRAY
+// IF YOU NEED TO BREAK OUT USE {{FOR OF}} 
+
+// ----------------FOR EACH WITH MAPS -----------------------------------------
+// const currencies = new Map([
+
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
+// WITH A MAP
+currencies.forEach(function(value, key, map){
+console.log(`${key}: ${value}`);
+});
+// USD: United States dollar
+// EUR: Euro
+// GBP: Pound sterling
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+// WITH A SET 
+const unique = new Set(['usd', "gbp", 'eur', 'usd', "gbp", 'eur']);
+unique.forEach(function(value, _, map) {
+  console.log(`${value}: ${_}`);
+});
+// usd: usd
+// gbp: gbp
+// eur: eur
+*/
 /////////////////////////////////////////////////
+
+
